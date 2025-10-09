d474f49341f7ca868fdc15726408c64cfd4d27a2
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2025-10-09 11:20:43 -0600

    Initial Commit

---
+++ theme-park-simulator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  theme-park-simulator
+//
+//  Created by Parker Morgan on 10/9/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
